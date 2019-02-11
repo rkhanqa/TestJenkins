23535ed422c3ed0115d5c0fc4a86d7aeb32f83e6
Rashid Khan-MacUser <rkhan.qa@gmail.com>
2019-02-11 13:55:45 +0000

    2ndCommit
    2ndCommit

---
+++ src/main/java/TestJenkins/App.java
@@ -0,0 +1,13 @@
+package TestJenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Wo2rld!" );
+    }
+}
